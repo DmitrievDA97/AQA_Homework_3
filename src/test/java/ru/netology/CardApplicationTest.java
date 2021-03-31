df342a31a07ccc8909b908b2954786c87f6ccbb6
Denis Dmitriev <denitos97@gmail.com>
2021-03-31 11:42:20 +0300

    Added negative tests

--- src/test/java/ru/netology/CardApplicationTest.java
+++ src/test/java/ru/netology/CardApplicationTest.java
@@ -21,6 +21,71 @@ public class CardApplicationTest {
 
     }
 
+    @Test
+    void emptyFields() {
+        open("http://localhost:9999");
+
+        $("[type=button]").click();
+        $(".input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+
+    }
+
+    @Test
+    void emptyNameField() {
+        open("http://localhost:9999");
+
+        $("[data-test-id=phone] input").setValue("+79500004455");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[data-test-id=name] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+
+    }
+
+    @Test
+    void emptyPhoneField() {
+        open("http://localhost:9999");
+
+        $("[data-test-id=name] input").setValue("Иванов Василий");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+
+    }
+    @Test
+    void emptyCheckbox() {
+        open("http://localhost:9999");
+
+        $("[data-test-id=name] input").setValue("Иванов Василий");
+        $("[data-test-id=phone] input").setValue("+79500004455");
+        $("[type=button]").click();
+        $(".input_invalid .checkbox__text").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+
+    }
+
+    @Test
+
+    void invalidName(){
+        open("http://localhost:9999");
+
+        $("[data-test-id=name] input").setValue("Ivanov Vasiliy");
+        $("[data-test-id=phone] input").setValue("+79500004455");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void invalidPhone() {
+        open("http://localhost:9999");
+
+        $("[data-test-id=name] input").setValue("Иванов Василий");
+        $("[data-test-id=phone] input").setValue("89500004455");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+
 
 
 
